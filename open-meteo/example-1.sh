0b05bb7e9a29c02a08a2650ee140fbfbfb1d5d40
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-11-28 17:53:09 +0000

    meteo

--- open-meteo/example-1.sh
+++ open-meteo/example-1.sh
@@ -1,12 +1,11 @@
+#!/bin/bash
 ULLCOORDS=$(source ipapi/example-1.sh)
 LATITUDE=$(echo $ULLCOORDS | jq '.latitude')
 LONGITUDE=$(echo $ULLCOORDS | jq '.longitude') 
-read -r -d '' PARAMS << EOM
-latitude=$LATITUDE
-&longitude=$LONGITUDE
-&current=temperature_2m,wind_speed_10m
-&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m
 EOM
-PARAMS=$(echo $PARAMS | tr -d '\n')
-#echo $PARAMS
-curl -s "https://api.open-meteo.com/v1/forecast?$PARAMS" | jq '.'
+curl -s -G "https://api.open-meteo.com/v1/forecast" \
+  --data-urlencode "latitude=$LATITUDE" \
+  --data-urlencode "longitude=$LONGITUDE"\
+  --data-urlencode 'current=temperature_2m,wind_speed_10m' \
+  --data-urlencode 'hourly=temperature_2m,relative_humidity_2m,wind_speed_10m' \
+  | jq '.'
